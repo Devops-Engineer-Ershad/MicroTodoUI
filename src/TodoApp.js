6db5b015dbf6bab95ffdc630e3dbfda30a98e919
devopsengineer-ershad <ershadalams150@gmail.com>
2025-10-20 15:33:33 +0530

    Update API base URLs for task management

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -6,9 +6,9 @@ import { Box } from '@mui/material';
 import config from './config'; // Import the config file with environment variable support
 
 // Use the config to define API URLs
-const GET_TASKS_API_BASE_URL = "http://10.0.60.211";
-const DELETE_TASK_API_BASE_URL = "http://10.0.83.167";
-const CREATE_TASK_API_BASE_URL = "http://10.0.232.67";
+const GET_TASKS_API_BASE_URL = "http://get-tasks.ak4t7.shop";
+const DELETE_TASK_API_BASE_URL = "http://delete-task.ak4t7.shop";
+const CREATE_TASK_API_BASE_URL = "http://add-task.ak4t7.shop";
 
 //Update Task Functionality is Work In Progress
 // const UPDATE_TASK_API_BASE_URL = '';
